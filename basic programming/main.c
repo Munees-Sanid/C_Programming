65287dbf355d0316f2db9f7df3f0f988cae0aa4a
Munees-Sanid <shanidshaan192@gmail.com>
2025-05-17 14:31:46 +0530

    update

---
+++ basic programming/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float r,area;
+    printf("enter the radius of the circle\n");
+    scanf("%f",&r);
+    area=3.14*r*r;
+    printf("the area of the circle is %0.2f",area);
+}
